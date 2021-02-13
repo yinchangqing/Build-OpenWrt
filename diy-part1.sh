2b201433f708578908f22b702ad3649e5fe5f6e6
yinchangqing <yinchangqing_@outlook.com>
2021-02-13 18:23:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,8 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
+
 #git clone https://github.com/vernesong/OpenClash -b master
 #cd OpenClash
 #cp -r luci-app-openclash/ $GITHUB_WORKSPACE/openwrt/package/diy/
